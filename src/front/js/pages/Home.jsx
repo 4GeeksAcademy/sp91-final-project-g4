4fe6b265c375347bc1ac0f03ae44798b2b36e2c6
Natalia Manzano Carnerero <mcn.sdevelop@gmail.com>
2025-02-26 16:22:38 +0000

    Home slides

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext.js";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import camiontransporte from "../../img/camiontransporte.webp";
+import clientesatisfecho from "../../img/clientesatisfecho.webp";
+import mapa from "../../img/mapa.png";
 import "../../styles/home.css";
 
 
@@ -8,20 +10,31 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-bs-ride="carousel">
+		<div className="carousel-indicators">
+		  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+		  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
 		</div>
+		<div className="carousel-inner">
+		  <div className="carousel-item active">
+		  <img src={camiontransporte} className="d-block w-100" alt="Slide 1" />
+		  </div>
+		  <div className="carousel-item">
+			<img src= {mapa}  className="d-block w-100" alt="Slide 2" />
+		  </div>
+		  <div className="carousel-item">
+			<img src= {clientesatisfecho}  className="d-block w-100" alt="Slide 3" />
+		  </div>
+		</div>
+		<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+		  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+		  <span className="visually-hidden">Previous</span>
+		</button>
+		<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+		  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+		  <span className="visually-hidden">Next</span>
+		</button>
+	  </div>
 	);
 };
